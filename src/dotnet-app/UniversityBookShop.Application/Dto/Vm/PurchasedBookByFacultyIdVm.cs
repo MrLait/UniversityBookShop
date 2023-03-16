c08f8a35bab4f52a6f76a191b0b21b06a8150d64
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-16 16:56:35 +0300

    GetPurchasedBooksByFacultyId added.

---
+++ src/dotnet-app/UniversityBookShop.Application/Dto/Vm/PurchasedBookByFacultyIdVm.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using UniversityBookShop.Application.Common.Mappings;
+using UniversityBookShop.Domain.Entities;
+
+namespace UniversityBookShop.Application.Dto.Vm;
+
+public class PurchasedBookByFacultyIdVm : IMapWith<PurchasedBookFaculty>
+{
+    public int Id { get; set; }
+    public int? BookId { get; set; }
+    public int? FacultyId { get; set; }
+    public BookDto? Book { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<PurchasedBookFaculty, PurchasedBookByFacultyIdVm>()
+        .ForMember(pb => pb.BookId, opt => opt.MapFrom(src => src.BookPurchasedBookFacultyId))
+        .ForMember(pb => pb.FacultyId, opt => opt.MapFrom(src => src.FacultyPurchasedBookFacultyId));
+    }
+}
