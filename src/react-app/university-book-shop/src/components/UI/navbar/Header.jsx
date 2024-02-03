90ac1389df4765bf4c1be2463fc5dcf1222efdbc
Lait <mr.lait3@gmail.com>
2024-02-03 03:50:11 +0300

    Header was fixed; Home page styles are changed;

--- src/react-app/university-book-shop/src/components/UI/navbar/Header.jsx
+++ src/react-app/university-book-shop/src/components/UI/navbar/Header.jsx
@@ -4,20 +4,24 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { routePaths } from '../../../router/routes';
 import styles from './Header.module.css'
-// className={styles.header}
 const Header = () => {
     return (
         <div className={styles.header}>
             <div className={styles.inner}>
-                {/* <Navbar bg="light" variant="light" > */}
                 <div className={styles.headerMain}>
-                    <Nav.Link href={routePaths.Universities}> Home </Nav.Link>
+                    <div className={styles.headerMainLeft} >
+                        University book shop app
+                    </div>
                     <div className={styles.headerBlank}></div>
                     <div className={styles.headerMainRight}>
-                        <Nav.Link href="ToDo">ToDo</Nav.Link>
+                        <Nav.Link className={styles.title} href={routePaths.Universities}>
+                            Home
+                        </Nav.Link>
+                        <Nav.Link className={styles.title} href="ToDo">
+                            ToDo
+                        </Nav.Link>
                     </div>
                 </div>
-                {/* </Navbar > */}
             </div>
         </div>
 
