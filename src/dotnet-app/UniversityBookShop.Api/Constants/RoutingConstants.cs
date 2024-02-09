783ca27052a93a2e87c75d2ff3973a79b0c9e90b
Lait <mr.lait3@gmail.com>
2024-02-09 21:17:10 +0300

    GetFacultyByIdQuery with its validator has added

--- src/dotnet-app/UniversityBookShop.Api/Constants/RoutingConstants.cs
+++ src/dotnet-app/UniversityBookShop.Api/Constants/RoutingConstants.cs
@@ -2,12 +2,18 @@
 {
     public static class RoutingConstants
     {
+        public static class Faculty{
+            public const string FacultyId = "{facultyId}";
+            public const string UniversityId = "university/{universityId}";
+        }
+
         public const string ApiController = "api/[controller]";
         public const string UniversityId = "{universityId}";
         public const string UniversityAndId = "university/{id}";
         public const string Id = "{id}";
         public const string FacultyId = "{facultyId}";
         public const string FacultyAndId = "faculty/{id}";
+        
         public const string FacultyIdBookId = "{facultyId}/{bookId}";
         public const string Add = "add/";
     }
